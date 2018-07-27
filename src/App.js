1f783baff847ff5a98ea0f04566217d491b254be
BenB <bblkmn5@gmail.com>
2018-07-26 22:15:53 -0700

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="navbar">
+          <h2>Navbar Placeholder</h2>
+        </div>
+        <div className="side-container">
+          <h2>Open Orders</h2>
+        </div>
+        <div className="main-container">
+          <h2>Support Options</h2>
+        </div> 
+        
       </div>
     );
   }
