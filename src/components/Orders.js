c54bbf00fb3d3905a192655debba147f67a9b4de
BenB <bblkmn5@gmail.com>
2018-08-21 10:25:44 -0700

    add Router and additional information to Orders

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Orders = ({ orders }) => {
   console.log(this.state)
   const renderOrders = orders.map(order =>
-    <p key={order.id}>{order.device}</p>
+    <Router>
+      <p key={order.id}>{order.device}-{order.location}-{order.service}-{order.notes}</p>
+    </Router>
   )
 
   return (
